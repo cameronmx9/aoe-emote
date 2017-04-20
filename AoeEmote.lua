d1ecb9e0e47cd2f2746e443b944bf56dcf337660
cameronmx9 <cameronmx9@hotmail.com>
2017-04-19 21:17:39 -0400

    added more func

--- AoeEmote.lua
+++ AoeEmote.lua
@@ -36,6 +36,33 @@ local function tableSize( ... )
 	return #tbl
 end
 
+-- options table
+-- first element as at position #1, not #0 like other languages
+local options = {5, 4, 3, 2, 1}
+
+-- selecting out of bounds just returns a nil value
+local n = options[1]
+
+--------------------------------------------
+-- table is two parts: Array + Hash Table --
+--------------------------------------------
+
+-- stored as a key in the hash table
+options["size"] = 10
+
+-- example option table
+local optionsTable = {
+	--array part:
+	1, 2, 3, 4, 5,
+	
+	--hash part:
+	["level"] = UnitLevel("player"),
+	["xOffset"] = 10,
+	["yOffset"] = 10,
+	["color"] = {r = 0.5, g = 0.2, b = 0.6, a = 1},
+	class = "Warrior",
+}
+
 local returnSimpleCounterValue = simpleCounter(6, 10, 9)
 local returnUnlimitedArgsCounter = unlimitedArgsCounter(6, 10, 9, 1)
 local tableSize = tableSize(1, 2, 3, 4, 5, "hello", "world")
@@ -43,3 +70,10 @@ local tableSize = tableSize(1, 2, 3, 4, 5, "hello", "world")
 print("counter: " .. returnSimpleCounterValue)
 print("unlimitedArgsCounter: " .. returnUnlimitedArgsCounter)
 print("tableSize: " .. tableSize)
+
+print("-----")
+
+print("optionsTable Info: \n")
+print("My Current Level: " ..  optionsTable["level"])
+print("My Class: " .. optionsTable.class)
+print("Table Size: " .. #optionsTable)
