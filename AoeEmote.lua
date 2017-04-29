9375414a3656e044be5fd81a68caee3b6b5e1e2b
cameronmx9 <cameronmx9@hotmail.com>
2017-04-28 22:51:17 -0400

    Renamed UIConfig to tilterFrame

--- AoeEmote.lua
+++ AoeEmote.lua
@@ -7,6 +7,7 @@ SlashCmdList.FRAMESTK = function()
 	FrameStackTooltip_Toggle()
 end
 
+SLASH_SHOWTILTER = "/tilter"
 
 -- to be able to use the left and right arrows in the edit box
 -- without rotating your character
@@ -27,13 +28,13 @@ CreateFrame Arguments:
 
 ]]
 -- returns a new instance of a frame
-local UIConfig = CreateFrame("Frame", "AoeEmote", UIParent, "BasicFrameTemplateWithInset");
+local tilterFrame = CreateFrame("Frame", "AoeEmote", UIParent, "BasicFrameTemplateWithInset");
 
 -- set UI size
-UIConfig:SetSize(300, 360);  -- width, height
+tilterFrame:SetSize(300, 360);  -- width, height
 
 -- set UI position: point, relativeFrame, relativePoint, xOffset, yOffset
-UIConfig:SetPoint("CENTER", UIParent, "LEFT", 150, 0);
+tilterFrame:SetPoint("CENTER", UIParent, "LEFT", 150, 0);
 
 --[[
  "CENTER could instead have been any of the following"
@@ -47,60 +48,60 @@ UIConfig:SetPoint("CENTER", UIParent, "LEFT", 150, 0);
 
 ]]
 
--- UIConfig is the parent frame for all other child frames and layers
+-- tilterFrame is the parent frame for all other child frames and layers
 -- regions we will ad on to it, to make it graphical
 
 -- Child frames and regions:
 
-UIConfig.title = UIConfig:CreateFontString(nil, "OVERLAY");
-UIConfig.title:SetFontObject("GameFontHighlight");
-UIConfig.title:SetPoint("LEFT", UIConfig.TitleBg, "LEFT", 5, 0);
-UIConfig.title:SetText("Tilter 9000");
+tilterFrame.title = tilterFrame:CreateFontString(nil, "OVERLAY");
+tilterFrame.title:SetFontObject("GameFontHighlight");
+tilterFrame.title:SetPoint("LEFT", tilterFrame.TitleBg, "LEFT", 5, 0);
+tilterFrame.title:SetText("Tilter 9000");
 
 -- UI Spit Button:
-UIConfig.spitButton = CreateFrame("Button", nil, UIConfig, "GameMenuButtonTemplate");
-UIConfig.spitButton:SetPoint("CENTER", UIConfig, "TOP", 0, -70);
-UIConfig.spitButton:SetSize(140, 40);
-UIConfig.spitButton:SetText("Spit");
-UIConfig.spitButton:SetNormalFontObject("GameFontNormalLarge");
-UIConfig.spitButton:SetHighlightFontObject("GameFontHighlightLarge");
-UIConfig.spitButton:SetScript("OnClick", function()
+tilterFrame.spitButton = CreateFrame("Button", nil, tilterFrame, "GameMenuButtonTemplate");
+tilterFrame.spitButton:SetPoint("CENTER", tilterFrame, "TOP", 0, -70);
+tilterFrame.spitButton:SetSize(140, 40);
+tilterFrame.spitButton:SetText("Spit");
+tilterFrame.spitButton:SetNormalFontObject("GameFontNormalLarge");
+tilterFrame.spitButton:SetHighlightFontObject("GameFontHighlightLarge");
+tilterFrame.spitButton:SetScript("OnClick", function()
     SendChatMessage("spits on you.", "EMOTE", nil, name)
 end
 )
     
 -- UI Cringe Button:
-UIConfig.cringeButton = CreateFrame("Button", nil, UIConfig, "GameMenuButtonTemplate");
-UIConfig.cringeButton:SetPoint("CENTER", UIConfig, "TOP", 0, -140);
-UIConfig.cringeButton:SetSize(140, 40);
-UIConfig.cringeButton:SetText("Cringe");
-UIConfig.cringeButton:SetNormalFontObject("GameFontNormalLarge");
-UIConfig.cringeButton:SetHighlightFontObject("GameFontHighlightLarge");
-UIConfig.cringeButton:SetScript("OnClick", function()
+tilterFrame.cringeButton = CreateFrame("Button", nil, tilterFrame, "GameMenuButtonTemplate");
+tilterFrame.cringeButton:SetPoint("CENTER", tilterFrame, "TOP", 0, -140);
+tilterFrame.cringeButton:SetSize(140, 40);
+tilterFrame.cringeButton:SetText("Cringe");
+tilterFrame.cringeButton:SetNormalFontObject("GameFontNormalLarge");
+tilterFrame.cringeButton:SetHighlightFontObject("GameFontHighlightLarge");
+tilterFrame.cringeButton:SetScript("OnClick", function()
     SendChatMessage("cringes at you.", "EMOTE", nil, name)
 end
 )
 
 -- UI Laugh Button:
-UIConfig.laughButton = CreateFrame("Button", nil, UIConfig, "GameMenuButtonTemplate");
-UIConfig.laughButton:SetPoint("CENTER", UIConfig, "TOP", 0, -210);
-UIConfig.laughButton:SetSize(140, 40);
-UIConfig.laughButton:SetText("Laugh");
-UIConfig.laughButton:SetNormalFontObject("GameFontNormalLarge");
-UIConfig.laughButton:SetHighlightFontObject("GameFontHighlightLarge");
-UIConfig.laughButton:SetScript("OnClick", function()
+tilterFrame.laughButton = CreateFrame("Button", nil, tilterFrame, "GameMenuButtonTemplate");
+tilterFrame.laughButton:SetPoint("CENTER", tilterFrame, "TOP", 0, -210);
+tilterFrame.laughButton:SetSize(140, 40);
+tilterFrame.laughButton:SetText("Laugh");
+tilterFrame.laughButton:SetNormalFontObject("GameFontNormalLarge");
+tilterFrame.laughButton:SetHighlightFontObject("GameFontHighlightLarge");
+tilterFrame.laughButton:SetScript("OnClick", function()
     SendChatMessage("laughs at you.", "EMOTE", nil, name)
 end
 )
 
 -- UI Fart Button:
-UIConfig.fartButton = CreateFrame("Button", nil, UIConfig, "GameMenuButtonTemplate");
-UIConfig.fartButton:SetPoint("CENTER", UIConfig, "TOP", 0, -280);
-UIConfig.fartButton:SetSize(140, 40);
-UIConfig.fartButton:SetText("Fart");
-UIConfig.fartButton:SetNormalFontObject("GameFontNormalLarge");
-UIConfig.fartButton:SetHighlightFontObject("GameFontHighlightLarge");
-UIConfig.fartButton:SetScript("OnClick", function()
+tilterFrame.fartButton = CreateFrame("Button", nil, tilterFrame, "GameMenuButtonTemplate");
+tilterFrame.fartButton:SetPoint("CENTER", tilterFrame, "TOP", 0, -280);
+tilterFrame.fartButton:SetSize(140, 40);
+tilterFrame.fartButton:SetText("Fart");
+tilterFrame.fartButton:SetNormalFontObject("GameFontNormalLarge");
+tilterFrame.fartButton:SetHighlightFontObject("GameFontHighlightLarge");
+tilterFrame.fartButton:SetScript("OnClick", function()
     SendChatMessage("farts on you.", "EMOTE", nil, name)
 end
 )
